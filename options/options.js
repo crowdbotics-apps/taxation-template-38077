57ab4f266ea59f56f34467cac7197a00e1fa60bc
Crowdbotics <team@crowdbotics.com>
2022-12-29 18:35:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "taxation_template_38077",
+  url: "https://taxation_template_38077.botics.co",
+  api: "https://taxation_template_38077.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
